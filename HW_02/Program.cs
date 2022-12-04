09c2fae7132651d7b24ab06f15ac224c207d63ad
YaOn999 <lumous999@gmail.com>
2022-12-04 21:49:42 +0300

    New HW_02

---
+++ HW_02/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 1000)
+{
+    while (number > 1000) 
+    {
+    number = number / 10;
+    }
+    int result = number % 10;
+    Console.WriteLine("третья цифра введенного числа " + result);
+}
+else
+{
+    Console.Write("третьей цифры нет");
+}
+
