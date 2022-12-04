425869b30ac5f9216e0e81b2e9a990ffcec2b4fc
YaOn999 <lumous999@gmail.com>
2022-12-04 21:36:29 +0300

    New HW_01

--- HW_01/Program.cs
+++ HW_01/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// {
+//     Console.Write("введите трехзначное число: ");
+//     number = int.Parse(Console.ReadLine());
+//     if (number < 100 || number > 999)
+//     {
+//         Console.WriteLine("нужно ввести трехзначное число!");
+//         continue;
+//     }
+//     break;
+// }
+
+Console.Clear();
+
+Console.Write("введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+int result = number / 10 % 10;
+Console.WriteLine("второе число числа " + number + " является " + result);
